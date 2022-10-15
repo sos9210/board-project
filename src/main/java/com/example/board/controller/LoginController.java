e401e6ee5d56ebdd61a3c4eba568973efdc836bc
sungsanghyun <sos9210@naver.com>
2022-10-15 23:04:50 +0900

    회원가입 완료(유효성검증 체크 및 메세지 출력) , 스프링시큐리티 의존성추가 및 스프링시큐리티설정 클래스파일 생성

--- src/main/java/com/example/board/controller/LoginController.java
+++ src/main/java/com/example/board/controller/LoginController.java
@@ -3,32 +3,58 @@ package com.example.board.controller;
 import com.example.board.domain.Member;
 import com.example.board.dto.MemberDTO;
 import com.example.board.enums.MemberAuthLevelEnum;
+import com.example.board.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
-@Controller
+@Controller @Slf4j
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final MemberService memberService;
+
     @GetMapping("board/user/login")
-    public String loginForm() {
+    public String memberLoginForm() {
+        return "view/loginForm";
+    }
+
+    @PostMapping("board/user/login")
+    public String memberLogin() {
+
         return "view/loginForm";
     }
 
     @GetMapping("board/user/member-join")
-    public String memberJoinForm() {
+    public String memberJoinForm(MemberDTO dto) {
         return "view/memberJoinForm";
     }
 
     @PostMapping("board/user/member-join")
-    public String memberJoin(HttpServletRequest request,MemberDTO dto) {
+    public String memberJoin(@Valid MemberDTO dto, Errors error,HttpServletRequest request) {
+        if(error.hasErrors()){
+            log.info("formError : {}",error.getFieldError().getDefaultMessage());
+            return "view/memberJoinForm";
+        }
         dto.setAuthLevel(MemberAuthLevelEnum.USER.name());
         Member member = new Member(dto.getMemberId(),dto.getMemberName(),dto.getPassword(),dto.getAuthLevel(), LocalDateTime.now(),request.getRemoteAddr());
 
+        try{
+            memberService.memberJoin(member);
+        }catch(IllegalArgumentException ex){
+            error.rejectValue("memberId","error.duplication.id");
+            return "view/memberJoinForm";
+        }
 
-        return "";
+        return "redirect:/board/user/login";
     }
 }
