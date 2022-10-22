35108bc050d93558bb07631a5d136eae00963436
sungsanghyun <sos9215@gmail.com>
2022-10-23 00:40:58 +0900

    테스트용파일추가, 첨부파일등록 테스트코드 작성 및 오류 수정

--- src/test/java/com/example/board/service/impl/BoardServiceImplTest.java
+++ src/test/java/com/example/board/service/impl/BoardServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.example.board.service.impl;
 
+import com.example.board.domain.AttachFile;
 import com.example.board.domain.Board;
 import com.example.board.domain.Member;
 import com.example.board.repository.AttachFileRepository;
@@ -11,11 +12,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.mock.web.MockMultipartHttpServletRequest;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
@@ -45,8 +59,35 @@ class BoardServiceImplTest {
 
         //when
         Long boardId = boardService.writeBoard(board,new MockMultipartHttpServletRequest());
+        //then
+        Assertions.assertEquals(board.getBoardSn(),boardId);
+    }
+    @Test
+    void 게시글_첨부파일_등록() throws IOException {
+        //given
+        Member member = new Member("asd123","user11","asd123!@#","1", LocalDateTime.now(),"127.0.0.1");
+        Board board = new Board("안녕하세요.","새로 가입한 홍길동 입니다.",member, "N",LocalDateTime.now(),"127.0.0.1");
+        ReflectionTestUtils.setField(board,"boardSn",1L);       //가짜 게시판순번 셋팅
+
+        MockMultipartHttpServletRequest request = new MockMultipartHttpServletRequest();
+
+        FileInputStream fileInputStream = new FileInputStream(System.getProperty("user.dir")+"/upload/test/test.txt");
+        MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt", "text/plain",fileInputStream );
+
+        String[] originalFilenameAndExt = multipartFile.getOriginalFilename().split("\\.");
+        String rootPath = System.getProperty("user.dir");
+
+        request.addFile(multipartFile);
+
+        given(boardRepository.save(board)).willReturn(board);
+
+        //when
+        List<AttachFile> fileSaves = ReflectionTestUtils.invokeMethod(boardService, "fileSave", board, request);
+        Long boardId = boardService.writeBoard(board,request);
 
         //then
         Assertions.assertEquals(board.getBoardSn(),boardId);
+        Assertions.assertEquals(fileSaves.get(0).getBoard().getBoardSn(), board.getBoardSn());
+        Assertions.assertEquals(fileSaves.get(0).getRealFileName(), "test");
     }
 }
