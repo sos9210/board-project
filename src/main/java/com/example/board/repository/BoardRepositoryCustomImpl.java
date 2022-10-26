ea1809811082caeee8c2c8a5362b1f15f86952c6
sungsanghyun <sos9210@naver.com>
2022-10-27 00:07:21 +0900

    목록화면 페이징처리중

--- src/main/java/com/example/board/repository/BoardRepositoryCustomImpl.java
+++ src/main/java/com/example/board/repository/BoardRepositoryCustomImpl.java
@@ -1,19 +1,21 @@
 package com.example.board.repository;
 
-import com.example.board.domain.Board;
 import com.example.board.domain.QBoard;
 import com.example.board.dto.BoardDTO;
 import com.example.board.dto.QBoardDTO;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
+import static com.example.board.domain.QBoard.*;
 
 @Repository
 public class BoardRepositoryCustomImpl implements BoardRepositoryCustom{
@@ -26,12 +28,12 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom{
 
     @Override
     public Page<BoardDTO> findByBoardList(BoardDTO search, Pageable pageable) {
-        QBoard board = QBoard.board;
         JPAQuery<Long> countQuery = queryFactory
                 .select(board.count())
                 .from(board)
                 .where(
-                        board.subject.eq(search.getSubject()).or(board.content.eq(search.getContent()))
+                        subjectEq(search.getSubject()),
+                        contentEq(search.getContent())
                 );
 
 
@@ -45,7 +47,8 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom{
                 ))
                 .from(board)
                 .where(
-                        board.subject.eq(search.getSubject()).or(board.content.eq(search.getContent()))
+                        subjectEq(search.getSubject()),
+                        contentEq(search.getContent())
                 )
                 .orderBy(board.registDate.desc())
                 .offset(pageable.getOffset())
@@ -53,5 +56,12 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom{
                 .fetch();
 
         return PageableExecutionUtils.getPage(boardList, pageable,  countQuery::fetchOne);
+    }
+
+    private Predicate subjectEq(String subject){
+        return StringUtils.hasText(subject) ? board.subject.eq(subject) : null;
+    }
+    private Predicate contentEq(String content){
+        return StringUtils.hasText(content) ? board.content.eq(content) : null;
     }
 }
